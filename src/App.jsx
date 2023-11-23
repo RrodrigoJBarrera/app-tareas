5960d3b53d20ccbdac8aad35aa8875ab674f7015
rrodrigojbarrera <rbarrera028@gmail.com>
2023-11-23 15:27:54 -0300

    clean app code

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import './App.css';
 
 import TaskContainer from './components/TaskContainer/TaskContainer.jsx';
 
-// const initTasks = [
-//   {
-//     id: 0,
-//     name: 'Example Task',
-//     complete: false,
-//   },
-// ];
-
 const App = () => {
   return <TaskContainer />;
 };
